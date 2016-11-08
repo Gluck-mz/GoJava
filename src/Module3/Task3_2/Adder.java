a20fdbe8e7059809c510434a69dd789b1ece25d7
Gluck-mz <aleksey.orishchenko@gmail.com>
2016-11-08 07:23:48 +0100

    Module 3. Task 3.2

---
+++ src/Module3/Task3_2/Adder.java
@@ -0,0 +1,4 @@
+package Module3.Task3_2;
+
+public class Adder {
+}
---
+++ src/Module3/Task3_2/Arithmetic.java
@@ -0,0 +1,7 @@
+package Module3.Task3_2;
+
+public class Arithmetic {
+        public static int add(int a, int b) {
+            return a + b;
+        }
+}
