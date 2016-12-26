b3db3f48ee82056813741ed7989f973875e1fdd6
Gluck-mz <aleksey.orishchenko@gmail.com>
2016-12-26 11:56:20 +0100

    Module 6. Task 6.5 [updated]

--- src/Module6/Task6_5/UserUtils.java
+++ src/Module6/Task6_5/UserUtils.java
@@ -2,28 +2,31 @@ package Module6.Task6_5;
 
 import Module6.Task6_4.User;
 
-public class UserUtils {
+import java.util.ArrayList;
+import java.util.List;
+
+public final class UserUtils {
+
     public static User[] uniqueUsers(User[] users) {
         User[] result = new User[users.length];
-        for (int i = 0; i < users.length - 1; i++) {
-            for (int j = i + 1; j < users.length; j++) {
-                if (!users[i].equals(users[j])) {
-                    result[i] = users[i];
-                } else {
-                    result[i] = null;
-                }
+        result[0] = users[0];
+        for (int i = 1; i < users.length; i++) {
+            if (!isUserPresent(users[i], result)) {
+                result[i] = users[i];
+            } else {
+                result[i] = null;
             }
         }
         return result;
     }
 
     public static User[] usersWithConditionalBalance(User[] users, int balance) {
-        User[] result = new User[users.length];
-        for (int i = 0; i < users.length; i++) {
-            if (users[i].getBalance() == balance) {
-                result[i] = users[i];
-            } else {
-                result[i] = null;
+        User[] result = new User[numberOfUsersWithBalance(users, balance)];
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < users.length; j++) {
+                if (users[j].getBalance() == balance) {
+                    result[i] = users[j];
+                }
             }
         }
         return result;
@@ -47,7 +50,7 @@ public class UserUtils {
     public static User[] deleteEmptyUsers(User[] users) {
         User[] result = new User[users.length];
         for (int i = 0; i < users.length; i++) {
-            if (users[i].getFirstName().equals(null) && users[i].getLastName().equals(null)) {
+            if (isUserEmpty(users[i])) {
                 result[i] = null;
             } else {
                 result[i] = users[i];
@@ -55,4 +58,37 @@ public class UserUtils {
         }
         return result;
     }
+
+    //created to simplify usersWithConditionalBalance() method
+    public static int numberOfUsersWithBalance(User[] users, int balance) {
+        int num = 0;
+        for (int i = 0; i < users.length; i++) {
+            if (users[i].getBalance() == balance) {
+                num++;
+            }
+        }
+        return num;
+    }
+
+    //created to simplify uniqueUsers() method
+    public static boolean isUserPresent(User user, User[] users) {
+        for (User u : users) {
+            if (user.equals(u)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //created to simplify deleteEmptyUsers() method
+    public static boolean isUserEmpty(User user) {
+        if (user.getFirstName() == null && user.getLastName() == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //private empty constructor
+    private UserUtils() {}
 }
